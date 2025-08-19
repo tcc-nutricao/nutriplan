0defc8697d71a216dca4a9e161c9b23cf1bcf400
Camila <camila.verssao@gmail.com>
2025-08-19 19:37:44 -0300

    chore: ajustes do docker

--- backend/start.sh
+++ backend/start.sh
@@ -2,7 +2,7 @@
 
 echo "Esperando o banco de dados ficar disponível..."
 # opcional: aguarda alguns segundos, ou use wait-for-it/dockerize em produção
-sleep 5
+./wait-for-it.sh db:3306 --timeout=30 --strict -- echo "Banco pronto!"
 
 echo "Aplicando migrations do Prisma..."
 npx prisma migrate deploy
