ed9acb22733d5723c3b23f6d8b789db5d587ae9b
Lívia De Araújo Nunes <liviaaraujonunes@gmail.com>
2025-09-28 15:47:51 -0300

    feat: register-personal-data frontend

---
+++ frontend/pages/register-personal-data.vue
@@ -0,0 +1,33 @@
+<template>
+
+ <div class="flex items-start justify-center w-full min-h-screen bg-gray-50 p-8">
+  <PersonalDataRegister
+  :initial-data="userData"
+  @submit="handleSubmit"
+  />
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+import PersonalDataRegister from "@/components/PersonalDataRegister.vue";
+
+// guardar dados do back
+const userData = ref(null);
+
+onMounted(async () => {
+  // chamar a API
+  const response = await fetch("/api/user/personal-data");
+  userData.value = await response.json();
+});
+
+function handleSubmit(formData) {
+  console.log("Dados recebidos do formulário:", formData);
+
+  //API de update
+  fetch("/api/user/personal-data", {
+    method: "POST",
+    body: JSON.stringify(formData),
+  });
+}
+</script>
